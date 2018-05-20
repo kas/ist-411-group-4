d650084f449d177a6e74788d5f1bb5e0dc6520cb
Kenneth Schnall <kenneth.schnall@gmail.com>
2018-05-19 20:02:47 -0700

    Add l01

---
+++ l01/411SimpleEchoServer/src/pkg411simpleechoserver/Server.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg411simpleechoserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        System.out.println("Simple Echo Server");
+        try (ServerSocket serverSocket = new ServerSocket(6000)) {
+            System.out.println("Waiting for connection.....");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connected to client");
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            clientSocket.getInputStream()));
+                    PrintWriter out = new PrintWriter(
+                            clientSocket.getOutputStream(), true)) {
+                String inputLine;
+                while ((inputLine = br.readLine()) != null) {
+                    System.out.println("Server: " + inputLine);
+                    out.println(inputLine);
+                }
+            }
+        }
+    }
+}
