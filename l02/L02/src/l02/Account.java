0beacadeab5d5cdf4b42ed6a621611d24a94b3bc
Kenneth Schnall <kenneth.schnall@gmail.com>
2018-05-23 21:05:34 -0700

    Add l02, add Account class

---
+++ l02/L02/src/l02/Account.java
@@ -0,0 +1,27 @@
+package l02;
+
+public class Account {
+  private int balance;
+
+  public Account(int balance) {
+    this.balance = balance;
+  }
+
+  public int deposit(int amount) {
+    this.balance += amount;
+    return balance;
+  }
+
+  public int withdraw(int amount) {
+    if (this.balance - amount < 0) {
+      return -1; // if a call to this method returned -1, then the withdrawal failed
+    }
+
+    this.balance -= amount;
+    return balance;
+  }
+
+  public int getBalance() {
+    return this.balance;
+  }
+}
---
+++ l02/L02/src/l02/L02.java
@@ -0,0 +1,7 @@
+package l02;
+
+public class L02 {
+    public static void main(String[] args) {
+
+    }
+}
