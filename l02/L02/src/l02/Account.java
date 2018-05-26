4b63182acf0df60d6881af5f35a45b0d328f2ea5
Kenneth Schnall <kenneth.schnall@gmail.com>
2018-05-26 11:46:23 -0700

    Add static transfer method

--- l02/L02/src/l02/Account.java
+++ l02/L02/src/l02/Account.java
@@ -24,4 +24,13 @@ public class Account {
   public int getBalance() {
     return this.balance;
   }
+
+  public static int transfer(Account account1, Account account2, int amount) {
+    if (account1.withdraw(amount) == -1) {
+      return -1; // if a call to this method returned -1, then the transfer failed
+    }
+
+    account2.deposit(amount);
+    return account2.getBalance();
+  }
 }
