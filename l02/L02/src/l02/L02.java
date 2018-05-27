165d6172da505a4cbdf5b8cb7f370597286a0000
bmorehouse33 <bfm5073@psu.edu>
2018-05-27 14:56:51 -0400

    create threads, add transfer methods

--- l02/L02/src/l02/L02.java
+++ l02/L02/src/l02/L02.java
@@ -1,7 +1,32 @@
 package l02;
 
 public class L02 {
+    
+    
     public static void main(String[] args) {
+        
+        Account accountOne = new Account(0);
+        Account accountTwo = new Account(0);
+        accountOne.deposit(200);
+        accountTwo.deposit(300);
+        
+        Thread t1 = new Thread(){
+            public void run(){
+                Account.transfer(accountOne, accountTwo, 300);
+                
+                
+                
+            }
+        };
+        Thread t2 = new Thread(){
+            public void run(){
+                Account.transfer(accountTwo, accountOne, 400);
 
+            }
+        };
+        t1.start();
+        t2.start();
+        System.out.println(accountOne.getBalance());
+        System.out.println(accountTwo.getBalance());
     }
-}
+    }
